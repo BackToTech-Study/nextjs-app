0c09055d27ee7a0d5fc0bc9518a6486e9f55aefd
Ionel Leuca <ionel.leuca@gmail.com>
2022-09-16 20:13:56 +0300

    added test endpoints

---
+++ pages/api/tests/index.ts
@@ -0,0 +1,33 @@
+import { Test } from "@prisma/client";
+
+import nc from "next-connect";
+import prisma from "../../../lib/server/prisma";
+import { ApiRequest, ApiResponse } from "../../../lib/server/types";
+
+export type GetTestsResponsePayload = { tests: Test[] };
+
+const getAll = async (
+  req: ApiRequest,
+  res: ApiResponse<GetTestsResponsePayload>
+) => {
+  const tests = await prisma.test.findMany();
+  prisma.$disconnect();
+
+  res.status(200).json({ status: true, tests });
+};
+
+export type CreateTestRequestPayload = Omit<Test, "id">;
+export type CreateTestResponsePayload = { test: Test };
+
+const create = async (
+  req: ApiRequest<CreateTestRequestPayload>,
+  res: ApiResponse<CreateTestResponsePayload>
+) => {
+  const test = await prisma.test.create({
+    data: req.body,
+  });
+
+  res.status(200).json({ status: true, test });
+};
+
+export default nc().get(getAll).post(create);
